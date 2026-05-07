07e91586a10813ae8bb4ad8d2d1489ddacb6d77a
siddGandhi <97721174+siddGandhi@users.noreply.github.com>
2026-05-07 14:27:56 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'workout-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/view.html',
+    '/analytics.html',
+    '/styles.css',
+    '/db-service.js',
+    '/firebase-config.js'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
